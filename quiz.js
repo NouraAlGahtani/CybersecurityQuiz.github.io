903b41e3b0315df6b1802345dbbb51d817060d1e
NouraAlGahtani <99655468+NouraAlGahtani@users.noreply.github.com>
2024-07-15 19:41:28 +0300

    well ...

---
+++ quiz.js
@@ -0,0 +1,162 @@
+// Defining questions
+var question1 = new question({
+	charName: "1. What does the acronym RSA stand for in cryptography?",
+	answerOptions: ["Rivest, Shamir, Adleman", "Random Security Algorithm", "Randomized Secure Authentication", "Rivest, Shamir, Algorithm"],
+	answer: 0,
+	pageNumber: "page-1"
+});
+
+var question2 = new question({
+	charName: "2. Which of the following is a symmetric key encryption algorithm?",
+	answerOptions: ["RSA", "ECC", "AES", "Diffie-Hellman"],
+	answer: 2,
+	pageNumber: "page-2"
+});
+
+var question3 = new question({
+	charName: "3. What is the main purpose of a cryptographic hash function?",
+	answerOptions: ["Encrypt data", "Generate a random number", "Verify data integrity", "Create a digital signature"],
+	answer: 2,
+	pageNumber: "page-3"
+});
+
+var question4 = new question({
+	charName: "4. Which of these is a type of attack on cryptographic algorithms?",
+	answerOptions: ["Birthday attack", "Sunset attack", "Rainbow attack", "Dawn attack"],
+	answer: 0,
+	pageNumber: "page-4"
+});
+
+var question5 = new question({
+	charName: "5. What is the primary characteristic of Elliptic Curve Cryptography (ECC)?",
+	answerOptions: ["Uses large key sizes", "Based on the difficulty of factoring large integers", "Provides strong security with smaller keys", "Uses symmetric keys"],
+	answer: 2,
+	pageNumber: "page-5"
+});
+
+//Defining prototype
+function question(option){
+	this.charName = option.charName;
+	this.answerOptions = option.answerOptions;
+	this.answer = option.answer;
+	this.pageNumber = option.pageNumber;
+}
+
+//Questions html template
+var genQuestion = function(x){
+	var stage = $('#questions');
+	stage.append('<div id="' + x.pageNumber + '" class="page"></div>');
+
+	var questionsPage = $('#' + x.pageNumber);
+	questionsPage.append('<h1>Cryptography Quiz</h1><hr/>');
+	questionsPage.append('<p class="charName">' + x.charName + '</p>');
+	questionsPage.append('<form>');
+	questionsPage.append('<input type="radio" name="tv1" value="0" checked>' + x.answerOptions[0] + '<br/>');
+	questionsPage.append('<input type="radio" name="tv1" value="1">' + x.answerOptions[1] + '<br/>');
+	questionsPage.append('<input type="radio" name="tv1" value="2">' + x.answerOptions[2] + '<br/>');
+	questionsPage.append('<input type="radio" name="tv1" value="3">' + x.answerOptions[3] + '<br/>');
+	questionsPage.append('</form>');
+	questionsPage.append('<button>next</button>');
+}
+
+//variables
+var count = 0;
+var nextPage = 1;
+
+// calculate score
+function showScore(){
+	$('.score').append(count + " out of 5");
+}
+
+//checking answer
+function checkAnswer(x){
+	var finalAnswer = $('input:checked').val();
+	if(nextPage == 5 && finalAnswer == x.answer){
+		count++;
+		$('#questions').hide();
+		$('#finish').show();
+		showScore();
+	} else if(nextPage == 5){
+		$('#questions').hide();
+		$('#finish').show();
+		showScore();
+	} else if(finalAnswer == x.answer){
+		count++;
+		nextPage++;
+		$('.page').hide();
+		$('#finish').hide();
+		$('#page-' + nextPage).show();
+	} else {
+		nextPage++;
+		$('.page').hide();
+		$('#finish').hide();
+		$('#page-' + nextPage).show();
+	}
+}
+
+//create a new game and questions
+function newGame(){
+	var create1 = new genQuestion(question1);
+	var create2 = new genQuestion(question2);
+	var create3 = new genQuestion(question3);
+	var create4 = new genQuestion(question4);
+	var create5 = new genQuestion(question5);
+}
+
+//restart game
+function restart(){
+	count = 0;
+	nextPage = 1;
+	$('#start-page').show();
+	$('#page-1').hide();
+	$('#page-2').hide();
+	$('#page-3').hide();
+	$('#page-4').hide();
+	$('#page-5').hide();
+	$('#finish').hide();
+	$('#questions').show();
+	$('.score').empty();
+	$('#finish').hide();
+}
+
+$(document).ready(function(){
+	// when the start button is clicked
+	$('#start-page button').click(function(){
+		$('#start-page').hide();
+		$('#page-1').show();
+		$('#page-2').hide();
+		$('#page-3').hide();
+		$('#page-4').hide();
+		$('#page-5').hide();
+		$('#finish').hide();
+	});
+
+	//generating the questions
+	newGame();
+
+	//events when next button is clicked
+	$('#page-1 button').click(function(){
+		checkAnswer(question1);
+	});
+
+	$('#page-2 button').click(function(){
+		checkAnswer(question2);
+	});
+
+	$('#page-3 button').click(function(){
+		checkAnswer(question3);
+	});
+
+	$('#page-4 button').click(function(){
+		checkAnswer(question4);
+	});
+
+	$('#page-5 button').click(function(){
+		checkAnswer(question5);
+	});
+
+	// event when try again is clicked
+	$('#finish button').click(function(){
+		restart();
+	});
+});
--- scriptt.js
+++ scriptt.js
@@ -1,171 +1,80 @@
-var canvas = document.getElementById("canvas")
-var ctx = canvas.getContext("2d")
+Vue.config.devtools = true;
 
-var width = 0
-var height = 0
+Vue.component("card", {
+  template: `
+    <div class="card-wrap"
+      @mousemove="handleMouseMove"
+      @mouseenter="handleMouseEnter"
+      @mouseleave="handleMouseLeave"
+      ref="card">
+      <div class="card"
+        :style="cardStyle">
+        <div class="card-bg" :style="[cardBgTransform, cardBgImage]"></div>
+        <div class="card-info">
+          <slot name="header"></slot>
+          <slot name="content"></slot>
+        </div>
+      </div>
+    </div>`,
+  mounted() {
+    this.width = this.$refs.card.offsetWidth;
+    this.height = this.$refs.card.offsetHeight;
+  },
+  props: ["dataImage"],
+  data: () => ({
+    width: 0,
+    height: 0,
+    mouseX: 0,
+    mouseY: 0,
+    mouseLeaveDelay: null
+  }),
 
-var iBubbles = 10
-
-var aBubblesX = []
-var aBubblesY = []
-var aBubblesSize = []
-var aBubblesSpeed = []
-var aZeroOrOne = []
-
-
-
-var random = (min, max) => {
-  if(typeof max === 'undefined') {
-    return Math.random() * (min[1] - min[0]) + min[0]
-  } else {
-    return Math.random() * (max - min) + min
-  }
-}
-
-
-
-class NumberElementConfig {
-  constructor(values, xMinMax, yMinMax, sizeMinMax, speedMinMax) {
-    this.values = values
-    this.x = xMinMax
-    this.y = yMinMax
-    this.size = sizeMinMax
-    this.speed = speedMinMax
-  }
-}
-
-class NumberElement {
-  constructor(numberElementConfig) {
-    this.config = numberElementConfig
-    this.spawn()
-  }
-  
-  spawn() {
-    this.value = this.config.values[Math.floor(random(0, 2))]
-    this.size = random(this.config.size)
-    this.speed = random(this.config.speed)
-    this.x = random(this.config.x)
-    this.y = random(this.config.y)
-    
-    this.viewportOffset = this.config.size[1] + 20
-    
-    if(Math.abs(this.speed) < 1) {
-      this.speed += (this.speed < 0 ? -1 : 1)
+  computed: {
+    mousePX() {
+      return this.mouseX / this.width;
+    },
+    mousePY() {
+      return this.mouseY / this.height;
+    },
+    cardStyle() {
+      const rX = this.mousePX * 30;
+      const rY = this.mousePY * -30;
+      return {
+        transform: `rotateY(${rX}deg) rotateX(${rY}deg)`
+      };
+    },
+    cardBgTransform() {
+      const tX = this.mousePX * -40;
+      const tY = this.mousePY * -40;
+      return {
+        transform: `translateX(${tX}px) translateY(${tY}px)`
+      };
+    },
+    cardBgImage() {
+      return {
+        backgroundImage: `url(${this.dataImage})`
+      };
     }
-    
-    this.setSpawnPosition()
-  }
-  
-  respawn() {
-    this.spawn()
-  }
-  
-  setSpawnPosition() {
-    if(this.speed < 0) {
-      let key = ['x', 'y'][Math.floor(random(0, 2))]
-      let value = key === 'x' ? innerWidth : innerHeight
-      this[key] = value + this.size
-    } else {
-      let key = ['x', 'y'][Math.floor(random(0, 2))]
-      this[key] = 0 - this.size
-    }
-  }
-  
-  isInViewport() {
-    if(
-      this.x < 0 - this.viewportOffset ||
-      this.y < 0 - this.viewportOffset ||
-      this.x > innerWidth + this.viewportOffset ||
-      this.y > innerHeight + this.viewportOffset
-    ) {
-      return false
-    } else {
-      return true
+  },
+
+  methods: {
+    handleMouseMove(e) {
+      this.mouseX = e.pageX - this.$refs.card.offsetLeft - this.width / 2;
+      this.mouseY = e.pageY - this.$refs.card.offsetTop - this.height / 2;
+    },
+    handleMouseEnter() {
+      clearTimeout(this.mouseLeaveDelay);
+    },
+    handleMouseLeave() {
+      this.mouseLeaveDelay = setTimeout(() => {
+        this.mouseX = 0;
+        this.mouseY = 0;
+      }, 1000);
     }
   }
-  
-  move() {
-    this.x += this.speed
-    this.y += this.speed
-  }
-}
+});
 
-class NumberElementCollector {
-
-  constructor(count = 100, spawnTimeout = 20) {
-    this.numberElements = []
-
-    let generator = () => {
-      if(count != this.numberElements.length) {
-        this.numberElements.push(
-          this.generateNumberElement()
-        )
-        setTimeout(generator, spawnTimeout)
-      }
-    }
-    generator()
-  }
-  
-  get() {
-    return this.numberElements
-  }
-  
-  moveAll() {
-    for (let i = 0; i < this.numberElements.length; ++i) {
-      let element = this.numberElements[i]
-      element.move()
-      if(!element.isInViewport()) {
-        element.respawn()
-      }
-    }
-  }
-
-  generateNumberElement() {
-    return new NumberElement(
-      new NumberElementConfig(
-        ['0', '1'],
-        [0, innerWidth], // x
-        [0, innerHeight], // y
-        [15, 50], // size
-        [-12, 12] // speed
-      )
-    )
-  }
-}
-
-
-var numberElementCollector = new NumberElementCollector()
-
-
-var draw = () => {
-  ctx.beginPath()
-  ctx.fillStyle = "rgba(0, 0, 0, 0.1)"
-  for (let numberElement of numberElementCollector.get()) {
-    ctx.font = numberElement.size / 1.5 + 'px arial black'
-    ctx.textBaseline = 'bottom'
-    ctx.textAlign = 'left'
-    ctx.fillText(
-      numberElement.value,
-      numberElement.x,
-      numberElement.y
-    )
-  }
-  ctx.fillStyle = "rgba(0, 0, 0, 0.07)"
-  ctx.fill()
-  ctx.lineWidth = 1
-  ctx.strokeStyle = "rgba(0, 0, 0, 0.08)"
-  ctx.stroke()
-}
-
-
-
-var process = () => {
-  width = canvas.width = innerWidth
-  height = canvas.height = innerHeight
-
-  numberElementCollector.moveAll()
-  draw()
-
-  requestAnimationFrame(process)
-}
-process()
+const app = new Vue({
+  el: "#app"
+});
+/** need to fix it doesnt work  */
